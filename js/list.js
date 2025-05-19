98e8c6f460b29fe29af59031fe07d97626454b9f
DGNFP <dgnam1110@gmail.com>
2025-05-19 22:13:24 +0900

    initial commit

---
+++ js/list.js
@@ -0,0 +1,6 @@
+// 게시글 목록 동작 스크립트
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("게시글 목록 페이지 로드됨");
+
+  // 필터 및 페이지네이션 기능 구현 예정
+});
---
+++ js/post.js
@@ -0,0 +1,6 @@
+// 게시글 상세 스크립트 (추후 작성)
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("게시글 상세 페이지 로드됨");
+
+  // 게시글 로딩 기능 구현 예정
+});
